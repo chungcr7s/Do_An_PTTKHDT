744143b75e89a6e9db2a6fee995bb47dd8fba339
chungcr7s <129494838+chungcr7s@users.noreply.github.com>
2024-05-16 17:29:31 +0700

    up

--- Economic/settings.py
+++ Economic/settings.py
@@ -79,13 +79,11 @@ WSGI_APPLICATION = 'Economic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME':'django_pj' ,
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
