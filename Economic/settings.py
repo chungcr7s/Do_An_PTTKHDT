2a6aaf1bdeb1df92f2a55c14fb66f5c97155a70c
chungcr7s <129494838+chungcr7s@users.noreply.github.com>
2024-05-29 13:55:17 +0700

    up

--- Economic/settings.py
+++ Economic/settings.py
@@ -77,15 +77,23 @@ WSGI_APPLICATION = 'Economic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME':'django_pj' ,
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME':'django_pj' ,
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # BASE_DIR sẽ tự động xác định đường dẫn đến thư mục dự án của bạn
     }
 }
+
 
 
 # Password validation
